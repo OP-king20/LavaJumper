4dfa4339f5b2757080026ccd30b00287c1e49d48
OP-king20 <samuel.pettersson@brevet.se>
2021-05-20 20:08:04 +0200

    Added Jump and refined Movement

--- LavaJumper/Assets/Scripts/PlayerMovement.cs
+++ LavaJumper/Assets/Scripts/PlayerMovement.cs
@@ -7,27 +7,63 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     float speed = 5f;
     [SerializeField]
-    float jumpSpeed = 5f;
+    float jumpForce = 5f;
 
     public Rigidbody2D rb;
+
+
+    bool isGrounded = false;
+    public Transform isGroundedChecker;
+    public float checkGroundRadius;
+    public LayerMask groundLayer;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Move();
+        Jump();
+        CheckIfGrounded();
+    }
+
+    private void Move()
+    {
         float inputX = Input.GetAxisRaw("Horizontal");
         float velocity = inputX * speed;
-        transform.Translate(Vector2.right * velocity * Time.deltaTime);
-
-
-        float inputY = Input.GetAxisRaw("Vertical");
-        float jump = inputY * jumpSpeed;
-        transform.Translate(Vector2.up * jump * Time.deltaTime);
-
+        rb.velocity = new Vector2(velocity, rb.velocity.y);
     }
+
+    private void Jump()
+    {
+        //Must be reworked
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            //float inputY = Input.GetAxisRaw("Vertical");
+            //float jump = inputY * jumpForce;
+            //rb.velocity = new Vector2(rb.velocity.x, jump);
+        }
+    }
+
+    void CheckIfGrounded()
+    {
+        Collider2D collider = Physics2D.OverlapCircle(isGroundedChecker.position, checkGroundRadius, groundLayer);
+        if (collider != null)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+
+
 }
+
